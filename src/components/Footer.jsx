693125a6e4637f5476382f9a6118093b453499eb
Abel <abel15655@gmail.com>
2021-11-03 19:59:59 +0100

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            &copy; Copyright 2021. Designed by <a href="http://">&#64;joshman</a>
+        </div>
+    );
+}
+ 
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <div className="header-logo"><span>D</span>ev-<span>G</span>uy</div>
+            <div className="contact">
+                <a href="mailto:ejembijoshman@gmail.com">Contact</a>
+            </div>
+        </header>
+    );
+}
+ 
+export default Header;
